545efbf24d76a5c294924d7a76904bac40bd3cda
rfv234 <gromdusha154@gmail.com>
2023-03-24 22:14:30 +0700

    tasks style

--- src/components/VueTasks.vue
+++ src/components/VueTasks.vue
@@ -1,21 +1,23 @@
 import ToDoItem from "./components/ToDoItem.vue";
 <template>
-    <form v-on:submit.prevent="addToDo">
-        <label for="newTask">Добавить задачу:</label><br>
-        <input id="newTask" placeholder="Новая задача" v-model="nextTitle">
-        <button>Добавить</button>
-    </form>
-    <ul>
-        <li
-            is="ToDoItem"
-            v-for="(todo, index) in todos"
-            v-bind:key="todo.id"
-            v-bind:title="todo.title"
-            v-on:remove="todos.splice(index, 1)"
-        >
-            {{todo.title}}
-        </li>
-    </ul>
+    <div id="glav">
+        <form v-on:submit.prevent="addToDo">
+            <label for="newTask">Добавить задачу:</label><br>
+            <input id="newTask" placeholder="Новая задача" v-model="nextTitle">
+            <button>Добавить</button>
+        </form>
+        <ul>
+            <li
+                is="ToDoItem"
+                v-for="(todo, index) in todos"
+                v-bind:key="todo.id"
+                v-bind:title="todo.title"
+                v-on:remove="todos.splice(index, 1)"
+            >
+                {{todo.title}}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -47,5 +49,15 @@ export default {
 </script>
 
 <style scoped>
-
+#glav {
+    width: 300px;
+    border: solid 1px;
+    position: absolute;
+    left: 45%;
+    top: 100px;
+    padding: 15px;
+    font-size: 17px;
+    background-color: lightblue;
+    border-radius: 10px;
+}
 </style>
